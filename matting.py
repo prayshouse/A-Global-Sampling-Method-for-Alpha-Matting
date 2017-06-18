7ac4753b69653911c6b76dac0b0b8538c06e84d8
prayshouse <3140103730@zju.edu.cn>
2017-06-18 21:55:11 +0800

    add remaining equation

--- matting.py
+++ matting.py
@@ -34,14 +34,41 @@ def get_estimate_alpha(i, f, b):
         return min(max(tmp1 * tmp2 / tmp3, 0), 1)
 
 
-def get_nearest_pixel(x, y, border_list, max_distance_square):
+# equation (3)
+def get_epsilon_c(i, f, b, alpha):
+    int_i = i.astype(int)
+    int_f = f.astype(int)
+    int_b = b.astype(int)
+    tmp1 = int_i - (alpha * int_f + (1-alpha) * int_b)
+    tmp2 = sum(tmp1 * tmp1)
+    return np.sqrt(tmp2)
+
+
+# equation (4)
+def get_epsilon_s(f_x, f_y, i_x, i_y, nearest):
+    tmp1 = (f_x - i_x) * (f_x - i_x) + (f_y - i_y) * (f_y - i_y)
+    return np.sqrt(tmp1 / nearest)
+
+
+# equation (5)
+def get_epsilon(i, f, b, f_x, f_y, b_x, b_y, i_x, i_y, nearest_f, nearest_b):
+    w = 1
+    alpha = get_estimate_alpha(i, g, b)
+    epsilon_c = get_epsilon_c(i, f, b, alpha)
+    epsilon_s_f = get_epsilon_s(f_x, f_y, i_x, i_y, nearest_f)
+    epsilon_s_b = get_epsilon_s(b_x, b_y, i_x, i_y, nearest_b)
+    return w * epsilon_c + epsilon_s_f + epsilon_s_b
+
+
+# calculate the nearest distance to border
+def get_nearest_distance(x, y, border_list, max_distance_square):
     nearest = max_distance_square
     for i in range(len(border_list)):
         tmp = (border_list[i][0] - x) * (border_list[i][0] - x) + (border_list[i][1] - y) * (border_list[i][1] - y)
         if nearest > tmp:
             nearest = tmp
             nearest_index = i
-    return nearest_index
+    return nearest
 
 
 # match the pixels in unknown areas with a sample
@@ -49,11 +76,19 @@ def sample_match(img, trimap, foreborder, backborder, unknown_seq):
     rows, cols, _ = img.shape
     sample = np.zeros((rows, cols, 4), dtype='int')
     for (i, j) in unknown_seq:
-        # print i
         sample[i, j, 0] = random.randint(0, len(foreborder))
         sample[i, j, 1] = random.randint(0, len(backborder))
-        sample[i, j, 2] = get_nearest_pixel(i, j, foreborder, rows * rows + cols * cols)
-        sample[i, j, 3] = get_nearest_pixel(i, j, backborder, rows * rows + cols * cols)
+        sample[i, j, 2] = get_nearest_distance(i, j, foreborder, rows * rows + cols * cols)
+        sample[i, j, 3] = get_nearest_distance(i, j, backborder, rows * rows + cols * cols)
+
+    for iteration in range(0, 5):
+        # propagation
+        for i in range(0, len(unknown_seq)):
+            # calculate epsilon among first order neighborhood
+            pass
+
+
+        # Random Search
 
     return sample
 
@@ -97,7 +132,7 @@ def main():
     # show(img, 3)
     # show(trimap)
 
-    alpha = matting(img, trimap)
+    #alpha = matting(img, trimap)
 
     # print img[0, 0][0]
     # i = [1,2 ,3]
@@ -106,11 +141,15 @@ def main():
     # print img[0, 0]
     # print img[0,1]
     # print img[0, 0] * img[0,1]
-    # print img[0,0].astype(int)*img[0,1].astype(int)
+    print img[0,0].astype(int)*0.3
+    print img[0, 0]
+    print img[0, 1]
+    print img[0, 2]
+    print get_epsilon_c(img[0, 0], img[0, 1], img[0, 0], 0.5)
     # print sum(img[0,0].astype(int)) / 3
     print type(img)
     print img.dtype
-    show(alpha)
+    # show(alpha)
 
 
 if __name__ == "__main__":
